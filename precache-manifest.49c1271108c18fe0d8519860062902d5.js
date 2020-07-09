8993a25578f63bcf239848a058111b6cb6784de1
Natalia <n.platova@hotmail.com>
2020-07-09 21:04:38 +0700

    Updates

---
+++ precache-manifest.49c1271108c18fe0d8519860062902d5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63ab2f534a7ed058d6ac8b2ad743fbd",
+    "url": "/planets/index.html"
+  },
+  {
+    "revision": "0b8d787b2f39ff9f8913",
+    "url": "/planets/static/css/main.aa6a0294.chunk.css"
+  },
+  {
+    "revision": "b64908501d376b016362",
+    "url": "/planets/static/js/2.c84824fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/planets/static/js/2.c84824fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8d787b2f39ff9f8913",
+    "url": "/planets/static/js/main.3cae4a14.chunk.js"
+  },
+  {
+    "revision": "d641ef7ebed34361ec0a",
+    "url": "/planets/static/js/runtime-main.5ea62729.js"
+  },
+  {
+    "revision": "a12230a1d7d94a57dacf50256a5a5212",
+    "url": "/planets/static/media/Earth.a12230a1.png"
+  },
+  {
+    "revision": "e1087eddd30479be0dfd7d514d9801f2",
+    "url": "/planets/static/media/Jupiter.e1087edd.png"
+  },
+  {
+    "revision": "abda3d31ee6eeb8e75a9bc1be5b89318",
+    "url": "/planets/static/media/Mars.abda3d31.png"
+  },
+  {
+    "revision": "e179e81a8a2bfc72c53938b6c8a3e0a2",
+    "url": "/planets/static/media/Mercury.e179e81a.png"
+  },
+  {
+    "revision": "465e28a40d35ad1d8e06d54039cfcba3",
+    "url": "/planets/static/media/Neptune.465e28a4.png"
+  },
+  {
+    "revision": "038a398d209e53f95826e3d43cae91bc",
+    "url": "/planets/static/media/Saturn.038a398d.png"
+  },
+  {
+    "revision": "428e288ddb8bad38d642c107d4d67a66",
+    "url": "/planets/static/media/Uranus.428e288d.png"
+  },
+  {
+    "revision": "7d68af0fcf4be99ed821b4e9dd6d916b",
+    "url": "/planets/static/media/Venus.7d68af0f.png"
+  },
+  {
+    "revision": "35b570683517d5e0323b7d0faad1b221",
+    "url": "/planets/static/media/background.35b57068.png"
+  },
+  {
+    "revision": "ed21aef570c36a32da178b04d2934cba",
+    "url": "/planets/static/media/background.ed21aef5.png"
+  },
+  {
+    "revision": "5a90768179db88151c957ca12ba281c7",
+    "url": "/planets/static/media/pluto-1315109_1920.5a907681.jpg"
+  },
+  {
+    "revision": "7a6c9bf303792e4e68920d3d46de65ac",
+    "url": "/planets/static/media/solar-system.7a6c9bf3.jpg"
+  }
+]);
